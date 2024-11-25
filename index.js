b6286b89f24040804b8e27c55f4a66ba1da5d61e
Ar <ar@Karens-MacBook-Pro.local>
2024-11-25 21:09:27 +0400

    homework is done

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let n = 5;
 for (let i = 1; i <= 16; i++) {
-   console.log(`${n} x ${i} = ${n * i} `);
+  console.log(`${n} x ${i} = ${n * i} `);
 }
+
+//////
+
+let sentence = "I am learning JavaScript";
+for (let i = 0; i < sentence.length; i++) {
+   sentence = sentence.replaceAll(" ", "-");
+ }
+ console.log(sentence)
